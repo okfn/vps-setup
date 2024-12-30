e93bea63d763816fc5f2e69f747c32f58de789e8
pdelboca <patriciodelboca@gmail.com>
2024-12-30 11:50:38 +0100

    First iteration of the script

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # vps-setup
 A script to execute common setup tasks on newly created VPS (Virtual Private Servers).
+
+## Description
+
+This script is based on Hetzner Community guides on [How to Keep a VPS Server Safe](https://community.hetzner.com/tutorials/security-ubuntu-settings-firewall-tools)
+
+This script is develop and tested in Debian 12, but probably will work on any Ubuntu as well.
---
+++ vps-setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Setup a Debian Virtual Private Server (VPS) hosted in Hetzner
+# This script follows the guidelines recommended by Hetzner:
+#  - https://community.hetzner.com/tutorials/setup-ubuntu-20-04
+#
+# Develop and tested in Debian 12 but should work on Ubuntu as well.
+
+update_apt() {
+    echo "######## Updating apt-get repository.."
+    apt update -q
+}
+
+setup_firewall() {
+  echo "########## Installing firewall and opening only ports 22, 80 and 443."
+  apt install -q ufw
+  ufw default deny incoming
+  ufw allow 22/tcp
+  ufw allow 80/tcp
+  ufw allow 443/tcp
+  ufw enable
+}
+
+disable_password_login() {
+  echo "########## Disabbling password authentication from SSH."
+  sed -i 's/#   PasswordAuthentication yes/   PasswordAuthentication no/' /etc/ssh/ssh_config
+  systemctl restart ssh
+}
+
+setup_fail2ban() {
+  echo "########## Installing and enabling fail2ban (using default configs)..."
+  apt install -q fail2ban
+  systemctl enable fail2ban
+  systemctl start fail2ban
+}
+
+setup_logwatch() {
+  echo "########## Installing and enabling logwatch (using default configs)"
+  apt install -q logwatch
+}
+
+install_utils() {
+  echo "########## Installing util tools like htop..."
+  apt install -q htop
+}
+
+main () {
+    update_apt
+    setup_firewall
+    disable_password_login
+    setup_fail2ban
+    setup_logwatch
+    install_utils
+}
+
+main
